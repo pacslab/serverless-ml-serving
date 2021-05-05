a281a48aa4c12822ffcc38152cd77ba52b50e5e5
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-05-05 03:25:31 +0000

    added configurable experiments

---
+++ experiments/README.md
@@ -0,0 +1,7 @@
+# Experimenter
+
+To run a jupyter notebook while recording the new outputs:
+
+```sh
+jupyter nbconvert --to notebook --inplace --execute experiment.ipynb
+```
--- experiments/helpers/util.py
+++ experiments/helpers/util.py
@@ -1,4 +1,9 @@
 import json
+from datetime import datetime
+import pytz
+import os
+
+my_timezone = os.getenv('PY_TZ', 'America/Toronto')
 
 def save_json_file(data, filename):
     with open(filename, 'w') as outfile:
@@ -15,3 +20,5 @@ def zero_if_none(inp):
     else:
         return inp
         
+def get_time_with_tz():
+    return datetime.now().astimezone(pytz.timezone(my_timezone))
