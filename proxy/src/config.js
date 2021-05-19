494363a3ed630afbb9fcaf66815821963b10d6a4
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-05-19 20:50:59 +0000

    changed upstream address to env variable

--- proxy/src/config.js
+++ proxy/src/config.js
@@ -1,21 +1,23 @@
+const KN_DOMAIN = process.env.KN_DOMAIN || 'kn.nima-dev.com'
+
 const workloadConfigs = {
   'bentoml-iris': {
     serviceName: 'bentoml-iris',
-    upstreamUrl: "http://bentoml-iris.default.kn.nima-dev.com/predict",
+    upstreamUrl: `http://bentoml-iris.default.${KN_DOMAIN}/predict`,
     maxBufferTimeoutMs: 1000,
     maxBufferSize: 3,
     isTFServing: false,
   },
   'tfserving-resnetv2': {
     serviceName: 'tfserving-resnetv2',
-    upstreamUrl: "http://tfserving-resnetv2.default.kn.nima-dev.com/v1/models/resnet:predict",
+    upstreamUrl: `http://tfserving-resnetv2.default.${KN_DOMAIN}/v1/models/resnet:predict`,
     maxBufferTimeoutMs: 1000,
     maxBufferSize: 3,
     isTFServing: true,
   },
   'bentoml-onnx-resnet50': {
     serviceName: 'bentoml-onnx-resnet50',
-    upstreamUrl: "http://bentoml-onnx-resnet50.default.kn.nima-dev.com/predict",
+    upstreamUrl: `http://bentoml-onnx-resnet50.default.${KN_DOMAIN}/predict`,
     maxBufferTimeoutMs: 1000,
     maxBufferSize: 3,
     isTFServing: false,
