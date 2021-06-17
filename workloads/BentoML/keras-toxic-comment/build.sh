ecdb73e34e4fa570d0d0dd0d55131a7bb1d862d5
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-06-16 20:07:16 -0400

    serving with py36 didnt work trying py38

---
+++ workloads/BentoML/keras-toxic-comment/build.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+# you need to do this only once when you download the dataset:
+# (cd tmp/data/ && unzip jigsaw-multilingual-toxic-comment-classification.zip)
+
+# you need to have activated conda env before running train script:
+# conda activate keras-toxic-comment
+python --version
+which python
+
+CLASSIFIER_NAME=ToxicCommentClassification
+DOCKER_TAG=$(date +%Y%m%d%H%M%S)
+DOCKER_IMAGE=ghcr.io/nimamahmoudi/bentoml-keras-toxic-comment-classification:$DOCKER_TAG
+
+python train.py
+
+# to serve:
+# bentoml serve ToxicCommentClassification:latest
+
+# package in a docker container
+bentoml containerize $CLASSIFIER_NAME:latest -t $DOCKER_IMAGE
+
+# print some info
+echo "Docker Image: $DOCKER_IMAGE"
+echo "command to test it out:"
+echo "docker run --rm -p 5000:5000 $DOCKER_IMAGE --workers=2"
+
+# test it out:
+# TEST_CONTAINER_NAME=bentoml-keras-toxic-comment-test
+# docker run --rm -d -p 5000:5000 --name $TEST_CONTAINER_NAME $DOCKER_IMAGE --workers=2
+
+# python test.py
+
+# docker stop $TEST_CONTAINER_NAME
---
+++ workloads/BentoML/keras-toxic-comment/toxic_comment_classifier.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from bentoml import api, artifacts, env, BentoService
+from bentoml.frameworks.keras import KerasModelArtifact
+from bentoml.service.artifacts.common import PickleArtifact
+from bentoml.adapters import DataframeInput, JsonOutput
+
+from tensorflow.keras.preprocessing import text, sequence
+import numpy as np
+import pandas as pd
+
+list_of_classes = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+max_text_length = 400
+
+@env(pip_packages=['tensorflow==1.14.0', 'keras==2.3.1', 'pandas', 'numpy'])
+@artifacts([PickleArtifact('x_tokenizer'), KerasModelArtifact('model')])
+class ToxicCommentClassification(BentoService):
+    
+    def tokenize_df(self, df):
+        comments = df['comment_text'].values
+        tokenized = self.artifacts.x_tokenizer.texts_to_sequences(comments)        
+        input_data = sequence.pad_sequences(tokenized, maxlen=max_text_length)
+        return input_data
+    
+    @api(input=DataframeInput(), output=JsonOutput(), batch=True)
+    def predict(self, df: pd.DataFrame) -> List[str]:
+        input_data = self.tokenize_df(df)
+        prediction = self.artifacts.model.predict(input_data)
+        result = []
+        for i in prediction:
+            result.append(list_of_classes[np.argmax(i)])
+        return result
