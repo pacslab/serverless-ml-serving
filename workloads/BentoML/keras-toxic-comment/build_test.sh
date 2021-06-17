505aa8d729a22e681d93dbf65bede4de98b04ed0
Nima Mahmoudi <nima_mahmoudi@live.com>
2021-06-16 22:51:57 -0400

    fixed the keras toxic issue and pushed it

--- workloads/BentoML/keras-toxic-comment/build_test.sh
+++ workloads/BentoML/keras-toxic-comment/build_test.sh
@@ -19,12 +19,16 @@ python train.py
 
 # package in a docker container
 bentoml containerize $CLASSIFIER_NAME:latest -t $DOCKER_IMAGE
-docker push $DOCKER_IMAGE
-
-# save for future usage
-echo -n $DOCKER_IMAGE > ./.dockerimage
 
 # print some info
 echo "Docker Image: $DOCKER_IMAGE"
 echo "command to test it out:"
 echo "docker run --rm -p 5000:5000 $DOCKER_IMAGE --workers=2"
+
+# test it out:
+# TEST_CONTAINER_NAME=bentoml-keras-toxic-comment-test
+# docker run --rm -d -p 5000:5000 --name $TEST_CONTAINER_NAME $DOCKER_IMAGE --workers=2
+
+# python test.py
+
+# docker stop $TEST_CONTAINER_NAME
